7ffedd0409c8fbf0137a45cd4186d1c06c82db3c
strelak56rus <48495413+strelak56rus@users.noreply.github.com>
2020-04-16 18:34:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,25 @@
 import telebot 
 import os
 
+from telebot import types
+
 token = os.environ.get('BOT_TOKEN')
 bot = telebot.TeleBot(str(token))
 
 @bot.message_handler(content_types=['text'])
+
 def lalala(message):
     bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(content_types=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton("Информация")
+    item2 = types.KeyboardButton("Наш ВКонтакте")
+    markup.add(item1, item2)
+    bot.send_message(message.chat.id, "Добро пожаловать, выбери нужную опцию.", reply_markup=markup)
+
+
 
 # RUN
 bot.polling()

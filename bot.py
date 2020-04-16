e32e987a119ffe2994d50108576f0e0282bff905
strelak56rus <48495413+strelak56rus@users.noreply.github.com>
2020-04-16 18:57:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,11 +10,17 @@ bot = telebot.TeleBot(str(token))
 @bot.message_handler(commands=['start'])
 def welcome(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1 = types.KeyboardButton("Информация")
-    item2 = types.KeyboardButton("Наш ВКонтакте")
+    item1 = types.KeyboardButton("❗️ Информация")
+    item2 = types.KeyboardButton("❗️ Наш ВКонтакте")
     markup.add(item1, item2)
     bot.send_message(message.chat.id, 'Добро пожаловать, выбери нужную опцию.', reply_markup=markup)
 
+def mainmenu(message):
+    if message.chat.type == 'private':
+        if message.text == '❗️ Информация':
+            bot.send_message(message.chat.id, "Привет")
+        elif message.text == '❗️ Наш ВКонтакте':
+            bot.send_message(message.chat.id, "https://vk.com/ccoin.shop")
 
 
 # RUN

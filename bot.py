4040496dda5d59894bff8b96491a3438ab719a2d
strelak56rus <48495413+strelak56rus@users.noreply.github.com>
2020-04-16 22:25:13 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,34 +15,34 @@ def start(message):
     item2 = types.KeyboardButton("📡 Наш ВКонтакте")
     item3 = types.KeyboardButton("💵 Профиль")
     markup.add(item1, item2, item3)
-    bot.send_message(message.chat.id, 'Добро пожаловать, выбери нужную опцию.', reply_markup=markup)
+    bot.send_message(message.chat.id, 'Добро пожаловать, выбери нужную кнопку.', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
-def mainmenu(message):
+def general(message):
     if message.chat.type == 'private':
         if message.text == '✉️ Информация':
-            bot.send_message(message.chat.id, "Привет, это игровой бот с валютой APPLECOIN, вы можете кликать эту валюту, и её можно обменять на CoronaCoin, и на другие валюты. Успей заработать много AC, и поделиться с друзьями!)")
+            bot.send_message(message.chat.id, "Информация: Это игровой бот с валютой APPLECOIN, вы можете кликать эту валюту, и её можно обменять на CoronaCoin, и на другие валюты. Успей заработать много AC, и поделиться с другими людьми!")
         elif message.text == '📡 Наш ВКонтакте':
-            bot.send_message(message.chat.id, "Наш ВКонтакте: https://vk.com/ccoin.shop")
+            bot.send_message(message.chat.id, "Мы в ВКонтакте: https://vk.com/ccoin.shop")
         
 
         elif message.text == '💵 Профиль':
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
             item1 = types.KeyboardButton("📈 Курс AppleCoin")
             item2 = types.KeyboardButton("🍏 Клик")
-            item3 = types.KeyboardButton("✅ Сохранить")
+            item3 = types.KeyboardButton("✅ Баланс")
             markup.add(item1, item2, item3)
             bot.send_message(message.chat.id, 'Привет, ты находишься в профиле, выбери то, что тебе нужно', reply_markup=markup)
         elif message.text == '📈 Курс AppleCoin':
             bot.send_message(message.chat.id, '1000 AC = 1 000 000 CoronaCoin | 1 руб = 5 000 AC')
         elif message.text == '🍏 Клик':
             bot.send_message(message.chat.id, 'В разработке')
-        elif message.text == '✅ Сохранить':
-            bot.send_message(message.chat.id, 'В разработке')
+        elif message.text == '✅ Баланс':
+            bot.send_message(message.chat.id, 'Баланс: 0 AC')
 
 
         else:
-            bot.send_message(message.chat.id, "Команда не найдена.")
+            bot.send_message(message.chat.id, "Ошибка, такая команда не существует.")
 
 
 # RUN
